02f75633438eb935307b7386f8ea8f489b7d1bd6
Grunet <sadikshahidain@gmail.com>
2022-02-06 22:09:47 +0000

    Remove the exception handling to be consistent with elsewhere

--- src/Contrib/Jaeger/JaegerSender.php
+++ src/Contrib/Jaeger/JaegerSender.php
@@ -40,60 +40,56 @@ class JaegerSender
      */
     public function send(array $spans): void
     {
-        try {
-            if (empty($spans)) {
-                return;
-            }
-
-            /** @var Tag[] $tags */
-            $tags = [];
-    
-            //TODO - determine what of this is still needed and how to adapt it for spec compliance
-            // foreach ($this->tracer->getTags() as $k => $v) {
-            //     if (!in_array($k, $this->mapper->getSpecialSpanTags())) {
-            //         if (strpos($k, $this->mapper->getProcessTagsPrefix()) !== 0) {
-            //             continue ;
-            //         }
-    
-            //         $quoted = preg_quote($this->mapper->getProcessTagsPrefix());
-            //         $k = preg_replace(sprintf('/^%s/', $quoted), '', $k);
-            //     }
-    
-            //     if ($k === JAEGER_HOSTNAME_TAG_KEY) {
-            //         $k = "hostname";
-            //     }
-    
-            //     $tags[] = new Tag([
-            //         "key" => $k,
-            //         "vType" => TagType::STRING,
-            //         "vStr" => $v
-            //     ]);
-            // }
-    
-            // $tags[] = new Tag([
-            //     "key" => "format",
-            //     "vType" => TagType::STRING,
-            //     "vStr" => "jaeger.thrift"
-            // ]);
-    
-            // $tags[] = new Tag([
-            //     "key" => "ip",
-            //     "vType" => TagType::STRING,
-            //     "vStr" => $this->tracer->getIpAddress()
-            // ]);
-    
-            $batch = new Batch([
-                "spans" => $spans,
-                "process" => new Process([
-                    "serviceName" => $this->serviceName,
-                    "tags" => $tags
-                ])
-            ]);
-    
-            $batch->write($this->protocol);
-            $this->protocol->getTransport()->flush();
-        } catch (\Exception $exception) {
-            $this->logError('Error exporting jaeger thrift spans over http', ['exception' => $exception->__toString()]);
+        if (empty($spans)) {
+            return;
         }
+
+        /** @var Tag[] $tags */
+        $tags = [];
+
+        //TODO - determine what of this is still needed and how to adapt it for spec compliance
+        // foreach ($this->tracer->getTags() as $k => $v) {
+        //     if (!in_array($k, $this->mapper->getSpecialSpanTags())) {
+        //         if (strpos($k, $this->mapper->getProcessTagsPrefix()) !== 0) {
+        //             continue ;
+        //         }
+
+        //         $quoted = preg_quote($this->mapper->getProcessTagsPrefix());
+        //         $k = preg_replace(sprintf('/^%s/', $quoted), '', $k);
+        //     }
+
+        //     if ($k === JAEGER_HOSTNAME_TAG_KEY) {
+        //         $k = "hostname";
+        //     }
+
+        //     $tags[] = new Tag([
+        //         "key" => $k,
+        //         "vType" => TagType::STRING,
+        //         "vStr" => $v
+        //     ]);
+        // }
+
+        // $tags[] = new Tag([
+        //     "key" => "format",
+        //     "vType" => TagType::STRING,
+        //     "vStr" => "jaeger.thrift"
+        // ]);
+
+        // $tags[] = new Tag([
+        //     "key" => "ip",
+        //     "vType" => TagType::STRING,
+        //     "vStr" => $this->tracer->getIpAddress()
+        // ]);
+
+        $batch = new Batch([
+            "spans" => $spans,
+            "process" => new Process([
+                "serviceName" => $this->serviceName,
+                "tags" => $tags
+            ])
+        ]);
+
+        $batch->write($this->protocol);
+        $this->protocol->getTransport()->flush();
     }
 }
