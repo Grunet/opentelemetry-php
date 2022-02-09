325445f6cd8e8950bcdbd14e7d2ac0aacb06d2a9
Grunet <sadikshahidain@gmail.com>
2022-02-09 04:15:04 +0000

    Remove all the commented out code

--- src/Contrib/Jaeger/CustomizedTHttpClient.php
+++ src/Contrib/Jaeger/CustomizedTHttpClient.php
@@ -56,50 +56,6 @@ class CustomizedTHttpClient extends THttpClient {
      */
     public function flush()
     {
-        // God, PHP really has some esoteric ways of doing simple things.
-        // $host = $this->host_ . ($this->port_ != 80 ? ':' . $this->port_ : '');
-
-        // $headers = array();
-        // $defaultHeaders = array('Host' => $host,
-        //     'Accept' => 'application/x-thrift',
-        //     'User-Agent' => 'PHP/THttpClient',
-        //     'Content-Type' => 'application/x-thrift',
-        //     'Content-Length' => TStringFuncFactory::create()->strlen($this->buf_));
-        // foreach (array_merge($defaultHeaders, $this->headers_) as $key => $value) {
-        //     $headers[] = "$key: $value";
-        // }
-
-        // $options = $this->context_;
-
-        // $baseHttpOptions = isset($options["http"]) ? $options["http"] : array(); //Unused by Otel
-
-        // $httpOptions = $baseHttpOptions + array('method' => 'POST',
-        //     'header' => implode("\r\n", $headers),
-        //     'max_redirects' => 1, //Relevant discussion around following redirects - https://github.com/guzzle/guzzle/issues/2584
-        //     // 'content' => $this->buf_
-        // );
-        // if ($this->timeout_ > 0) {
-        //     $httpOptions['timeout'] = $this->timeout_; //Unused by Otel
-        // }
-        // $this->buf_ = '';
-
-        // $options["http"] = $httpOptions;
-        // $contextid = stream_context_create($options);
-        // $this->handle_ = @fopen(
-        //     $this->scheme_ . '://' . $host . $this->uri_,
-        //     'r',
-        //     false,
-        //     $contextid
-        // );
-
-        // // Connect failed?
-        // if ($this->handle_ === false) {
-        //     $this->handle_ = null;
-        //     $error = 'THttpClient: Could not connect to ' . $host . $this->uri_;
-        //     throw new TTransportException($error, TTransportException::NOT_OPEN);
-        // }
-
-        //SOS - in progress rewrite below
         $request = $this->requestFactory->createRequest('POST', $this->endpointUrl);
 
         $headers = [
